a1a6e701b158a119453e40d69a951446f0b08767
christianlasam <christianlasam1@gmail.com>
2026-02-23 14:16:21 -0800

    Set up random levels for training

--- record_gif_cnn.py
+++ record_gif_cnn.py
@@ -14,6 +14,7 @@ MODELS_DIR = "./models/"
 REPLAYS_DIR = "./replays/"
 
 from gym_super_mario_bros.smb_env import SuperMarioBrosEnv
+from gym_super_mario_bros.smb_random_stages_env import SuperMarioBrosRandomStagesEnv
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from nes_py.wrappers import JoypadSpace
 from shimmy.openai_gym_compatibility import GymV21CompatibilityV0
@@ -31,7 +32,8 @@ if not os.path.exists(model_path + ".zip"):
     print(f"Make sure your model is inside the '{MODELS_DIR}' folder.")
     exit()
 
-env = SuperMarioBrosEnv(rom_mode="vanilla", lost_levels=False, target=(1, 1))
+#env = SuperMarioBrosEnv(rom_mode="vanilla", lost_levels=False, target=(1, 1))
+env = SuperMarioBrosRandomStagesEnv()
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 env = GymV21CompatibilityV0(env=env, render_mode="rgb_array")
 env = apply_wrappers(env)
@@ -61,5 +63,5 @@ os.makedirs(REPLAYS_DIR, exist_ok=True)
 save_path = os.path.join(REPLAYS_DIR, f"{model_name}.gif")
 
 print(f"Saving replay to {save_path}...")
-frames[0].save(save_path, save_all=True, append_images=frames[1:], duration=33, loop=0)
+frames[0].save(save_path, save_all=True, append_images=frames[1:], duration=66, loop=0)
 print("Done!")
--- train_cnn.py
+++ train_cnn.py
@@ -1,11 +1,15 @@
 import gymnasium as gym
+import torch
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import CheckpointCallback
+import faulthandler
 import os
 import time
 
+faulthandler.enable()
+torch.cuda.empty_cache()
 # generate a short timestamp
 timestamp = time.strftime("%m%d_%H%M")
 run_name = f"mario_cnn_{timestamp}"
@@ -15,13 +19,13 @@ logs_dir = "./logs/"
 os.makedirs(models_dir, exist_ok=True)
 os.makedirs(logs_dir, exist_ok=True)
 
-from gym_super_mario_bros.smb_env import SuperMarioBrosEnv
+from gym_super_mario_bros.smb_random_stages_env import SuperMarioBrosRandomStagesEnv
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from nes_py.wrappers import JoypadSpace
 from shimmy.openai_gym_compatibility import GymV21CompatibilityV0
 from wrappers import apply_wrappers
 
-env = SuperMarioBrosEnv(rom_mode="vanilla", lost_levels=False, target=(1, 1))
+env = SuperMarioBrosRandomStagesEnv()
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 env = GymV21CompatibilityV0(env=env)
 env = apply_wrappers(env)
@@ -29,23 +33,26 @@ env = Monitor(env)
 env = DummyVecEnv([lambda: env])
 
 # init PPO Model with CNN
+
 model = PPO(
     "CnnPolicy",
     env,
     verbose=1,
-    learning_rate=0.0001,
-    n_steps=2048,
-    batch_size=64,
-    n_epochs=10,
-    gamma=0.99,
-    gae_lambda=0.95,
+    learning_rate=0.0002,
+    batch_size=256,
+    n_steps=8192,
+    n_epochs=15,
+    ent_coef=0.001,
+    clip_range = 0.1,
+    target_kl=0.03,
     tensorboard_log=logs_dir,
     device="auto",
 )
 
+#model = PPO.load("./models/mario_cnn_0212_2136_final.zip", env=env, tensorboard_log=logs_dir, device="auto")
 # save model every 50,000 steps so we don't lose progress
 checkpoint_callback = CheckpointCallback(
-    save_freq=50000, save_path=models_dir, name_prefix=run_name
+    save_freq=100000, save_path=models_dir, name_prefix=run_name
 )
 
 print("------------------------------------------")
@@ -54,7 +61,7 @@ print(f"  Models saving to: {models_dir}")
 print("------------------------------------------")
 
 try:
-    model.learn(total_timesteps=1000000, callback=checkpoint_callback)
+    model.learn(total_timesteps=20000000, callback=checkpoint_callback, reset_num_timesteps=False)
 
     final_path = os.path.join(models_dir, f"{run_name}_final")
     model.save(final_path)
